60dd4e58fe80c3c0bff5625e2f51d68ef09ef782
GuiMeiring <guilhermemeiring08@gmail.com>
2023-09-04 17:46:37 -0300

    main

---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:clean_architecture/layers/presentations/ui/pages/carro_page.dart';
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(MaterialApp(
+    home: CarroPage(),
+  ));
+}
